75788b73e01cbd8f3fec04ed9313601d26d87983
Meryem Sekkat <meryem@pre-history.com>
2021-09-10 18:09:20 +0200

    feat: integration of daisyui

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,49 @@
 <template>
-  <Tutorial/>
+  <div>
+    <div class="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box">
+      <div class="flex-none">
+        <button class="btn btn-square btn-ghost">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+          </svg>
+        </button>
+      </div>
+      <div class="flex-1 px-2 mx-2">
+    <span class="text-lg font-bold">
+            With two icons
+          </span>
+      </div>
+      <div class="flex-none">
+        <button class="btn btn-square btn-ghost">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
+          </svg>
+        </button>
+      </div>
+    </div>
+    <footer class="p-10 footer bg-neutral text-neutral-content">
+      <div>
+        <span class="footer-title">Services</span>
+        <a class="link link-hover">Branding</a>
+        <a class="link link-hover">Design</a>
+        <a class="link link-hover">Marketing</a>
+        <a class="link link-hover">Advertisement</a>
+      </div>
+      <div>
+        <span class="footer-title">Company</span>
+        <a class="link link-hover">About us</a>
+        <a class="link link-hover">Contact</a>
+        <a class="link link-hover">Jobs</a>
+        <a class="link link-hover">Press kit</a>
+      </div>
+      <div>
+        <span class="footer-title">Legal</span>
+        <a class="link link-hover">Terms of use</a>
+        <a class="link link-hover">Privacy policy</a>
+        <a class="link link-hover">Cookie policy</a>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script lang="ts">
---
+++ pages/test.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+.test
+  input( type="checkbox" checked="checked" class="toggle")
+</template>
+
+<script>
+export default {
+  name: "test"
+}
+</script>
+
+<style scoped>
+
+</style>
