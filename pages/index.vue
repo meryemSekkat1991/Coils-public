d52c6fc2370e2b81776bb62a43c8791bd7e20317
Meryem Sekkat <meryem@pre-history.com>
2021-09-10 22:46:56 +0200

    feat: add stylint to coils-front

--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,34 @@
-<template>
-  <div>
-    <div class="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box">
-      <div class="flex-none">
-        <button class="btn btn-square btn-ghost">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
-          </svg>
-        </button>
-      </div>
-      <div class="flex-1 px-2 mx-2">
-    <span class="text-lg font-bold">
-            With two icons
-          </span>
-      </div>
-      <div class="flex-none">
-        <button class="btn btn-square btn-ghost">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
-          </svg>
-        </button>
-      </div>
-    </div>
-    <footer class="p-10 footer bg-neutral text-neutral-content">
-      <div>
-        <span class="footer-title">Services</span>
-        <a class="link link-hover">Branding</a>
-        <a class="link link-hover">Design</a>
-        <a class="link link-hover">Marketing</a>
-        <a class="link link-hover">Advertisement</a>
-      </div>
-      <div>
-        <span class="footer-title">Company</span>
-        <a class="link link-hover">About us</a>
-        <a class="link link-hover">Contact</a>
-        <a class="link link-hover">Jobs</a>
-        <a class="link link-hover">Press kit</a>
-      </div>
-      <div>
-        <span class="footer-title">Legal</span>
-        <a class="link link-hover">Terms of use</a>
-        <a class="link link-hover">Privacy policy</a>
-        <a class="link link-hover">Cookie policy</a>
-      </div>
-    </footer>
-  </div>
+<template lang="pug">
+.navbar.mb-2.shadow-lg.bg-neutral.text-neutral-content.rounded-box
+  .flex-none
+    button.btn.btn-square.btn-ghost
+      svg.inline-block.w-6.h-6.stroke-current(xmlns='http://www.w3.org/2000/svg' fill='none' viewbox='0 0 24 24')
+        path(stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M4 6h16M4 12h16M4 18h16')
+  .flex-1.px-2.mx-2
+    span.text-lg.font-bold
+      | With two icons
+  .flex-none
+    button.btn.btn-square.btn-ghost
+      svg.inline-block.w-6.h-6.stroke-current(xmlns='http://www.w3.org/2000/svg' fill='none' viewbox='0 0 24 24')
+        path(stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z')
+  footer.p-10.footer.bg-neutral.text-neutral-content
+    div
+      span.footer-title Services
+      a.link.link-hover Branding
+      a.link.link-hover Design
+      a.link.link-hover Marketing
+      a.link.link-hover Advertisement
+    div
+      span.footer-title Company
+      a.link.link-hover About us
+      a.link.link-hover Contact
+      a.link.link-hover Jobs
+      a.link.link-hover Press kit
+    div
+      span.footer-title Legal
+      a.link.link-hover Terms of use
+      a.link.link-hover Privacy policy
+      a.link.link-hover Cookie policy
 </template>
 
 <script lang="ts">
