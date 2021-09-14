e0950e1190c6268d2f7a877e64168fc067e95e8d
Meryem Sekkat <meryem@pre-history.com>
2021-09-14 17:48:59 +0200

    feat: implement warehouses pages

---
+++ pages/warehouses.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+.warehouses
+  p {{warehouses}}
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "nuxt-property-decorator";
+
+@Component({
+  name: "warehouses"
+})
+
+export default class home extends Vue {
+  warehouses = [];
+  mounted () {
+    this.$axios
+      .get('/api/admin/warehouses')
+      .then(res => (
+        this.warehouses = res.data
+      ));
+  }
+}
+</script>
