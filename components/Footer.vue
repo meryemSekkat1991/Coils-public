4379b44d2fac31364483d5dc77de392b86b31fc6
meryem sekkat <34552218+meryemSekkat1991@users.noreply.github.com>
2021-09-16 16:29:40 +0200

    style: change color (#16)

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-footer.p-4.footer.text-base-content.footer-center(class="bg-gradient-to-b from-yellow-500 via-yellow-300 to-yellow-200")
+footer.p-4.footer.text-base-content.footer-center(class="bg-gradient-to-b from-indigo-500 via-indigo-300 to-indigo-200")
   div
     p Copyright &copy; 2021 - All right reserved by ACME Industries Ltd
 </template>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-div(class="navbar mb-2 shadow-lg bg-gradient-to-b from-yellow-500 via-yellow-300 to-yellow-500 text-white")
+div(class="navbar mb-2 shadow-lg bg-gradient-to-b from-indigo-500 via-indigo-300 to-indigo-100 text-white")
   .flex-none(class='lg:flex')
     label.btn.btn-square.btn-ghost(:for='id')
       svg.inline-block.w-6.h-6.stroke-current(xmlns='http://www.w3.org/2000/svg' fill='none' viewbox='0 0 24 24')
@@ -11,7 +11,7 @@ div(class="navbar mb-2 shadow-lg bg-gradient-to-b from-yellow-500 via-yellow-300
     .avatar.mr-2
       .rounded-full.w-10.h-10.m-1
         img(src='https://i.pravatar.cc/500?img=32')
-    button.btn.bg-yellow-300.text-white.font-bold.border-yellow-100(@click="logout" class="hover:bg-yellow-400 hover:border-yellow-100") logout
+    button.btn.bg-indigo-300.text-white.font-bold.border-indigo-100(@click="logout" class="hover:bg-indigo-400 hover:border-indigo-100") logout
 </template>
 
 <script lang="ts">
