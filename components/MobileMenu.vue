2a761d5e1e00e0032ef1247e650b6b30418a3521
meryem sekkat <34552218+meryemSekkat1991@users.noreply.github.com>
2021-09-16 12:43:16 +0200

    feat: logout (#14)

--- components/MobileMenu.vue
+++ components/MobileMenu.vue
@@ -1,16 +1,16 @@
 <template lang="pug">
 .drawer-side
   label.drawer-overlay(:for='sidebar')
-  ul.p-4.overflow-y-auto.menu.w-80.shadow-lg(class="bg-gradient-to-br from-yellow-100 via-yellow-300 to-yellow-500")
-    li
+  ul.p-4.overflow-y-auto.menu.w-80.shadow-lg.text-white(class="bg-gradient-to-bl from-yellow-100 via-yellow-300 to-yellow-500")
+    li(class="mb-2")
       nuxt-link(to="/") Home
-    li
+    li(class="mb-2")
       nuxt-link(to="/warehouse") Warehouses
-    li
+    li(class="mb-2")
       nuxt-link(to="/products") Products
-    li
+    li(class="mb-2")
       nuxt-link(to="/services") Services
-    li
+    li(class="mb-2")
       nuxt-link(to="/profiles") Profiles
 </template>
 
