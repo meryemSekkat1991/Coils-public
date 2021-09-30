1773bb564e807f4f1e76693b08569ea9b593b8fe
meryem sekkat <34552218+meryemSekkat1991@users.noreply.github.com>
2021-09-30 16:13:44 +0200

    fix: breadcrumbs design and lang switcher loading (#22)

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -1,13 +1,13 @@
 <template lang="pug">
 .dropdown
-  .m-1.btn.bg-transparent.border-none.shadow(tabindex='0' class="hover:bg-indigo-300") Lang
+  .m-1.btn.bg-transparent.border-none.shadow(tabindex='0' class="hover:bg-indigo-300") {{currentLang}}
   ul( tabindex="0" class="p-2 shadow menu dropdown-content bg-gradient-to-b from-indigo-200 via-indigo-300 to-indigo-100 rounded-box w-auto top-16")
     li(v-for="locale in availableLocales" :key="locale.code")
       nuxt-link(:to="switchLocalePath(locale.code)") {{ locale.name }}
 </template>
 
 <script lang="ts">
-import { Vue, Component} from "nuxt-property-decorator";
+  import {Vue, Component, Watch} from "nuxt-property-decorator";
 
 @Component({
   name: "LanguageSwitcher",
@@ -15,7 +15,15 @@ import { Vue, Component} from "nuxt-property-decorator";
     availableLocales () {
       return this.$i18n.locales.filter((lang: { code: string; }) => lang.code !== this.$i18n.locale)
     }
-  }
+  },
 })
-export default class LanguageSwitcher extends Vue {}
+export default class LanguageSwitcher extends Vue {
+  get currentLang(): string {
+    return this.$i18n.locale;
+  }
+  @Watch("$route")
+  routeChanged(): void {
+    location.reload();
+  }
+}
 </script>
---
+++ vue-shim.d.ts
@@ -0,0 +1,4 @@
+declare interface Breadcrumb {
+  text: string;
+  href: string;
+}
